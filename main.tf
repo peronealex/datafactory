18f07a6cb1e11cc19f1cf985c8ef01ef45c7efb9
Alessio Perone <alessio.perone@lexisnexisrisk.com>
2020-12-21 16:08:43 +0000

    detaching the config

--- main.tf
+++ main.tf
@@ -7,19 +7,7 @@ resource "azurerm_data_factory" "example" {
 
 /*
 #########################testing begin
-/*resource "azurerm_data_factory_linked_service_sql_server" "example" {
-  name                = var.name
-  resource_group_name = var.resource_group_name
-  data_factory_name   = var.name
-  connection_string   = "Server=tcp:alessiodatalake.database.windows.net,1433;Initial Catalog=alessiodatalake;Persist Security Info=False;User ID=alessiodatalake;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
-}
-
-resource "azurerm_data_factory_dataset_sql_server_table" "example" {
-  name = var.name
-  resource_group_name = var.resource_group_name
-  data_factory_name   = var.name
-  linked_service_name = var.name
-}*/
+/**/
 ######################################testing end
 
 /* working!!!!!
@@ -45,6 +33,6 @@ resource "azurerm_data_factory_dataset_json" "example" {
   resource_group_name = var.resource_group_name
   data_factory_name = var.name
   linked_service_name = var.
-  
+
 }
 */
