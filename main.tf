bd0010b23c7312310a9c972de84eb8b12523ca1d
Alessio Perone <alessio.perone@lexisnexisrisk.com>
2020-12-21 15:28:13 +0000

    extract config datafactory

--- main.tf
+++ main.tf
@@ -4,29 +4,8 @@ resource "azurerm_data_factory" "example" {
   resource_group_name = var.resource_group_name
   tags                = var.tags
 }
+
 /*
-resource "azurerm_data_factory_pipeline" "example" {
-  name                = var.name
-  resource_group_name = var.resource_group_name
-  data_factory_name   = var.name
-  variables = {
-    "bob" = "item1"
-  }
-  activities_json = <<JSON
-[
-    {
-        "name": "Append variable1",
-        "type": "AppendVariable",
-        "dependsOn": [],
-        "userProperties": [],
-        "typeProperties": {
-            "variableName": "bob",
-            "value": "something"
-        }
-    }
-]
-  JSON  
-}/*
 #########################testing begin
 /*resource "azurerm_data_factory_linked_service_sql_server" "example" {
   name                = var.name
